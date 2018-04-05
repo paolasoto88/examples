b746fc4b01c46b864ba1a073ee0ee167ca95ac04
paolasoto88 <37549327+paolasoto88@users.noreply.github.com>
2018-04-05 16:16:41 +0200

    Update runps.sh

--- tf/runps.sh
+++ tf/runps.sh
@@ -1,5 +1,1 @@
-python /tfdata/mnist_distributed.py --ps_hosts=tensorflow-ps-service.default.svc.cluster.local:2222 \
---worker_hosts=tensorflow-wk-service0.default.svc.cluster.local:2222,tensorflow-wk-service1.default.svc.cluster.local:2222 \
---job_name=ps \
---task_index=0 \
---data_dir=/tfdata/data02  --train_dir=/tfdata/checkpoint02/1
+python /tfdata/mnist_distributed.py --ps_hosts=tensorflow-ps-service.default.svc.cluster.local:2222 --worker_hosts=tensorflow-wk-service0.default.svc.cluster.local:2222,tensorflow-wk-service1.default.svc.cluster.local:2222 --job_name=ps --task_index=0 --data_dir=/tfdata/data02  --train_dir=/tfdata/checkpoint02/1
