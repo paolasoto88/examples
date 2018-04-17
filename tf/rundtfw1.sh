91295426c0cac3e0c4cb7ffc499216b8647b50b4
paolasoto88 <37549327+paolasoto88@users.noreply.github.com>
2018-04-17 16:46:32 +0200

    Create rundtfw1.sh

---
+++ tf/rundtfw1.sh
@@ -0,0 +1,1 @@
+python /tfdata/demo_model.py --worker_hosts=tensorflow-wk-service0.default.svc.cluster.local:2222,tensorflow-wk-service1.default.svc.cluster.local:2222 --job_name=worker --task_index=1
