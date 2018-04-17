1537fcf4e74d71d39341a25b4f337fc58f8117f8
paolasoto88 <37549327+paolasoto88@users.noreply.github.com>
2018-04-17 16:45:57 +0200

    Update rundtfw0.sh

--- tf/rundtfw0.sh
+++ tf/rundtfw0.sh
@@ -1,3 +1,1 @@
-python demo_model.py \
-     --worker_hosts=localhost:2224,localhost:2225 \
-     --job_name=worker --task_index=0
+python /tfdata/demo_model.py --worker_hosts=tensorflow-wk-service0.default.svc.cluster.local:2222,tensorflow-wk-service1.default.svc.cluster.local:2222 --job_name=worker --task_index=0
